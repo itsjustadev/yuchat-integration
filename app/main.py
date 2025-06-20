fec1887cd631c86c7ba07cc16d5d29dc87b688d1
Nahasapeemapetilon Cetheres <itsjustfortwits@gmail.com>
2025-06-20 11:17:35 +0300

    syntax editions

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from classes import TelegramBot
 
 message: str = (
     f"*CI/CD Build Report*\n\n"
-    f"*Build URL:* [Открыть сборку]({os.getenv('BUILD_URL')})\n"
+    f"*Build URL:* [{os.getenv('BUILD_URL')}]({os.getenv('BUILD_URL')})\n"
     f"*Branch:* `{os.getenv('BRANCH')}`\n"
     f"*Repository:* `{os.getenv('REPO')}`\n"
     f"*User:* `{os.getenv('USER')}`\n"
     f"*Workflow:* `{os.getenv('WORKFLOW_NAME')}`\n\n"
-    f"*Jobs*\n"
+    f"*Jobs:*\n"
     f"*Lint:* {os.getenv('LINT_RESULT')}\n"
     f"*Deploy:* {os.getenv('DEPLOY_RESULT')}\n"
     f"*Get Info:* {os.getenv('GET_INFO_RESULT')}"
